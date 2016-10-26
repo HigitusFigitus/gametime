85ebf51ff7d842c812635938cde9b6ab4630cec5
lokpio <lokpio322@gmail.com>
2016-10-26 15:08:41 -0700

    Create all the controllers

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,9 @@
+class FriendshipsController < ApplicationController
+	def index
+		
+	end
+
+	def create
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only:[:show] do
+  	resources :friendships, only:[:create,:index]
+  	resources :favorites , only:[:create,:index]
+  end
+
+  resources :games, only:[:index,:show] do
+  	resources :comments, only:[:index,:new,:create]
+  end
+  
+  resources :tags, only:[:show]
+
+  root "games#index"
 end
