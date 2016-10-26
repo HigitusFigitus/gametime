3ad539c7a1f53b1b09aacd360c746c309cf70d92
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-26 16:50:22 -0700

    updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
 require 'json'
 
-Tag.create(name: "Board Game")
-5.times do |i|
+Tag.destroy_all
+Game.destroy_all
+
+default_tag = Tag.create(name: "Party Game")
+
+100.times do |i|
   response = JSON.parse(HTTParty.get("https://bgg-json.azurewebsites.net/thing/#{i+1}"))
 
-  if response['mechanics'] != []
-    tag = Tag.create(name: response['mechanics'][0])
+  if response['mechanics']
+    tag = Tag.find_or_create_by(name: response['mechanics'][0])
   end
 
-  Game.create(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], vote: 12, tag_id: tag ? tag.id : 1)
+  id = tag ? tag.id : default_tag.id
+
+  Game.create(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], vote: 12, tag_id: id)
 end
