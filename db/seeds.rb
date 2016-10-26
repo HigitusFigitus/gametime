ca0c5938cc59e88d21c7679714bcad7af65568df
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-26 15:39:27 -0700

    adds working seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+Tag.create(name: "Board Game")
+5.times do |i|
+  response = JSON.parse(HTTParty.get("https://bgg-json.azurewebsites.net/thing/#{i+1}"))
+
+  if response['mechanics'] != []
+    tag = Tag.create(name: response['mechanics'][0])
+  end
+
+  Game.create(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], vote: 12, tag_id: tag ? tag.id : 1)
+end
