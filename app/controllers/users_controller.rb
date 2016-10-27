c85577f99b791697ef6317e5da937e6337d7564e
King Heng Jonathan Lok <email": null>
2016-10-26 21:21:24 -0700

    Add the add friends feature, have not figure out how to do the private method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 	def show
 		@user = User.find(params[:id])
     @games = @user.games
+    @friends = @user.friends
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,10 @@ default_tag = Tag.create(name: "Party Game")
 end
 
 User.create(username:"lol",email:"123@gmail.com",password:"123123",password_confirmation:"123123")
+User.create(username:"foo",email:"456@gmail.com",password:"123123",password_confirmation:"123123")
+User.create(username:"boo",email:"789@gmail.com",password:"123123",password_confirmation:"123123")
+
+Friendship.create(user_id:1,friend_id:2)
 Favorite.create(user_id:1,game_id:1)
 Favorite.create(user_id:1,game_id:2)
 Favorite.create(user_id:1,game_id:3)
