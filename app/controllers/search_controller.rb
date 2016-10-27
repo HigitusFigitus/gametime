95639e62ace8b9da7a78d573bb63ba44b2a13381
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-27 16:20:45 -0700

    implements functional search bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,4 +9,24 @@ class SearchController < ApplicationController
   def lower(str)
     str.downcase
   end
+
+  def user_search(keyword)
+    results = []
+    results << User.where("lower(email) like ?", "%#{keyword}%") if    User.where("lower(email) like ?", "%#{keyword}%").count > 0
+
+    results << User.where("lower(first_name) like ?", "%#{keyword}%") if User.where("lower(first_name) like ?", "%#{keyword}%").count > 0
+
+    results << User.where("lower(last_name) like ?", "%#{keyword}%") if User.where("lower(last_name) like ?", "%#{keyword}%").count > 0
+
+    results << User.where("lower(username) like ?", "%#{keyword}%") if User.where("lower(username) like ?", "%#{keyword}%").count > 0
+
+    return results
+  end
+
+  def game_search(keyword)
+    results = []
+    results << Game.where("lower(name) like ?", "%#{keyword}%") if    Game.where("lower(name) like ?", "%#{keyword}%").count > 0
+
+    return results
+  end
 end
