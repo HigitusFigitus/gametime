a848208532ee4102702e8384874b9dd520cac4fd
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-26 22:23:10 -0700

    cleans up games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,5 @@
 class GamesController < ApplicationController
 	def index
-		# @games = Game.all
-		# @tags = Game.group(:tag_id).order('count_tag_id desc').count(:tag_id)
 		@tags = Tag.all
 	end
 
