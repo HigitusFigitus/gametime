8b8700437f53e0c5d8c4b1f96c8fcc9dddd481ea
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-26 20:56:31 -0700

    final seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,12 @@ default_tag = Tag.create(name: "Party Game")
 
 100.times do |i|
   response = JSON.parse(HTTParty.get("https://bgg-json.azurewebsites.net/thing/#{i+1}"))
-  p "https://bgg-json.azurewebsites.net/thing/#{i+1}"
 
-  if response['mechanics']
-    tag = Tag.find_or_create_by(name: response['mechanics'][0])
+  if !response['message']
+    tag = Tag.find_or_create_by(name: response['mechanics'][0]) if response['mechanics'] && response['mechanics'] != []
+
+    Game.create!(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], vote: 12, tag_id: tag ? tag.id : default_tag.id)
   end
 
-  id = tag ? tag.id : default_tag.id
-
-  Game.create(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], vote: 12, tag_id: id)
+  puts "#{i+1}: #{tag ? tag.id : default_tag.id} #{response['name'] ? response['name'] : '*** SKIP ***'}"
 end
