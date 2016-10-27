5acca293b6329a81ae2a3256f34671302bd52b65
King Heng Jonathan Lok <lokpio322@gmail.com>
2016-10-26 17:11:12 -0700

    Update 20161026191113_create_favorites.rb
    stupid sam fault

--- db/migrate/20161026191113_create_favorites.rb
+++ db/migrate/20161026191113_create_favorites.rb
@@ -2,7 +2,7 @@ class CreateFavorites < ActiveRecord::Migration[5.0]
   def change
     create_table :favorites do |t|
       t.integer :game_id
-      t.integer :user_ids
+      t.integer :user_id
 
       t.timestamps
     end
