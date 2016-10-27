66137c7ef173bb43376260801594e51ffbc05d5d
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-27 12:00:25 -0700

    adds functional navbar and a little style

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20161026192254) do
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "username",                            null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
