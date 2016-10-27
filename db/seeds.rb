1d8f6f43e6c24c9e01582e03b688e339fcef049b
King Heng Jonathan Lok <email": null>
2016-10-26 17:13:10 -0700

    Half way in the user show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Game.destroy_all
 
 default_tag = Tag.create(name: "Party Game")
 
-100.times do |i|
+20.times do |i|
   response = JSON.parse(HTTParty.get("https://bgg-json.azurewebsites.net/thing/#{i+1}"))
 
   if response['mechanics']
@@ -16,3 +16,9 @@ default_tag = Tag.create(name: "Party Game")
 
   Game.create(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], vote: 12, tag_id: id)
 end
+
+User.create(username:"lol",email:"123@gmail.com",password:"123123",password_confirmation:"123123")
+Favorite.create(user_id:1,game_id:1)
+Favorite.create(user_id:1,game_id:2)
+Favorite.create(user_id:1,game_id:3)
+
