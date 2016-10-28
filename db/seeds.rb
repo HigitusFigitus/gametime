7cdec4fbbcc847a4c1d5cc08409bbe855116bbf0
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-27 20:57:30 -0700

    adds faker gem and some fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,21 @@ require 'json'
 
 default_tag = Tag.create(name: "Trick-taking")
 
-User.create(username:"lol",email:"123@gmail.com",password:"123123",password_confirmation:"123123")
-User.create(username:"foo",email:"456@gmail.com",password:"123123",password_confirmation:"123123")
-User.create(username:"boo",email:"789@gmail.com",password:"123123",password_confirmation:"123123")
-
-20.times do |i|
+2.times do |i|
   response = JSON.parse(HTTParty.get("https://bgg-json.azurewebsites.net/thing/#{i+1}"))
 
   if !response['message']
     tag = Tag.find_or_create_by(name: response['mechanics'][0]) if response['mechanics'] && response['mechanics'] != []
 
-    Game.create!(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], tag_id: tag ? tag.id : default_tag.id)
+    Game.find_or_create_by(name: response['name'], image_url: response['image'], thumbnail_url: response['thumbnail'], min_players: response['minPlayers'], max_players: response['maxPlayers'], play_time: response['playingTime'], publish_year: response['yearPublished'], description: response['description'], tag_id: tag ? tag.id : default_tag.id)
 
   end
 
   puts "#{i+1}: #{tag ? tag.id : default_tag.id} #{response['name'] ? response['name'] : '*** SKIP ***'}"
 end
 
-Friendship.create(user_id:1,friend_id:2)
-Favorite.create(user_id:1,game_id:1)
-Favorite.create(user_id:1,game_id:2)
-Favorite.create(user_id:1,game_id:3)
+urls = ["https://i.imgur.com/i9k8D6W.jpg", "https://i.imgur.com/KHLSocG.jpg", "https://i.imgur.com/VqIiZ25.jpg", "https://i.imgur.com/dRxnay8.jpg", "https://i.imgur.com/LnG12CJ.jpg"]
+
+5.times do |i|
+  User.create!(email: "Faker::Internet.email", username: Faker::Internet.user_name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, image_url: urls[i])
+end
